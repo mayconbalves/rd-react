2751d6dafdd2f1130453bf15756cd4af483d362d
mayconbalves <mayconmustaine@hotmail.com>
2020-06-04 18:30:42 -0300

    feat(container): create all content and styled of component

--- src/components/container/ContentContainer.js
+++ src/components/container/ContentContainer.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 
+import DesktopImg from '../../assets/desktop-responsive-design.png'
+import TabletImg from '../../assets/tablets-responsive-design.png'
+import MobileImg from '../../assets/mobile-responsive-design.png'
+
 const ParagraphSection = styled.section`
   margin: 5% 10%;
   h2 {
@@ -35,6 +39,79 @@ const Circle = styled.div`
   display: inline-block;
 `
 
+const ImageSection = styled.section`
+  display: grid;
+  grid-template-columns: 31% 31% 31%;
+  grid-column-gap: 3%;
+`
+
+const BoxImg = styled.div`
+  background: ${props => props.background};
+  max-height: 330px;
+
+  .desktop {
+    background: #FE9481;
+
+    button {
+      background: ${props => props.backgroud};
+    }
+  }
+  .tablet {
+    background: #FCDA92;
+  }
+  .mobile {
+    background: #9C8CB9;
+  }
+
+
+  img {
+    background: ${props => props.background};
+    margin: 0 20%;
+  }
+
+  div {
+    background:  #fff;
+    min-height: 112px;
+  }
+
+  h3 {
+    background: ${props => props.background};
+    color: #fff;
+    font-size: 1.77777777778em;
+    font-weight: 300;
+    margin: 30px 0;
+    text-align: center;
+  }
+
+  p {
+    color: #868686;
+    background: #fff;
+    float: left;
+    height: 100px;
+    padding: 1.9444444444444445%;
+    max-width: 100%;
+    text-align: justify;
+    top: 225px;
+  }
+`
+
+const StyledButton = styled.button`
+  background: ${props => props.background};
+  border: none;
+  height: 40px;
+  font-size: 1.1111111111em;
+  font-weight: 400;
+  float: right;
+  margin: 10px;
+  text-align: center;
+  text-shadow: none;
+  width: 33.33333333333333%;
+  color: #fff;
+  padding: 0 10px;
+  position: relative;
+  bottom: 50px;
+`
+
 class ContentContainer extends Component {
   constructor(props) {
     super(props)
@@ -58,9 +135,46 @@ class ContentContainer extends Component {
           </h3>
         </ParagraphSection>
 
-        <section>
+        <ImageSection>
+          <BoxImg background='#FE9481'>
+            <div className='desktop'>
+              <img src={DesktopImg} alt={DesktopImg} background='#FE9481' />
+              <h3>Site Responsivo desktop</h3>
+            </div>
+            <div>
+              <p>
+                Quando pressionado o botão Leia mais... o restante da informação deverá aparecer em scroll down.
+              </p>
+              <StyledButton background='#FE9481'>Leia mais...</StyledButton>
+            </div>
+          </BoxImg>
 
-        </section>
+          <BoxImg background='#FCDA92'>
+            <div className='tablet'>
+              <img src={TabletImg} alt={TabletImg} background='#FCDA92' />
+              <h3>Site Responsivo tablet</h3>
+            </div>
+            <div>
+              <p>
+                Quando pressionado o botão Leia mais... informação deverá aparecer completa em um popup na tela.
+              </p>
+              <StyledButton background='#FCDA92'>Leia mais...</StyledButton>
+            </div>
+          </BoxImg>
+
+          <BoxImg background='#9C8CB9'>
+            <div className='mobile'>
+              <img src={MobileImg} alt={MobileImg} background='#9C8CB9' />
+              <h3>Site Responsivo mobile</h3>
+            </div>
+            <div>
+              <p>
+                Quando pressionado o botão alterar tema modifique o tema do site para blackfriday a seu gosto.
+              </p>
+              <StyledButton background='#9C8CB9'>Alterar tema</StyledButton>
+            </div>
+          </BoxImg>
+        </ImageSection>
       </div>
     )
   }
