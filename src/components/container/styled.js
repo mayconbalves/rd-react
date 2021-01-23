8419c32650904459ad6df5bf3648eef5e298f7d5
mayconbalves <mayconmustaine@hotmail.com>
2021-01-22 22:45:29 -0300

    refactor(container): remove duplicated styled

--- src/components/container/styled.js
+++ src/components/container/styled.js
@@ -85,12 +85,3 @@ export const BoxImg = styled.div`
     font-weight: 800;
   }
 `
-
-export const CircleDiv = styled.div`
-  height: 20px;
-  width: 20px;
-  background: ${(props) => props.background || '#000'};
-  border-radius: 50%;
-  display: inline-block;
-  margin: 0 5px;
-`
