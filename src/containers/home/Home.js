77b3acdcfb23dca8f910b35f24ca8932f57dbd9d
mayconbalves <mayconmustaine@hotmail.com>
2021-01-29 22:33:38 -0300

    refactor(*): remove container component

--- src/containers/home/Home.js
+++ src/containers/home/Home.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
 import Menu from 'components/menu'
-import ContentContainer from 'components/container'
 import Footer from 'components/footer/Footer'
-import { Container } from './Styled'
+import Circle from 'components/coloredCircle'
+import Button from 'components/button'
+import { Container, ParagraphSection, BoxImg, ImageSection } from './styled'
+
+import DesktopImg from 'assets/desktop-responsive-design.png'
+import TabletImg from 'assets/tablets-responsive-design.png'
+import MobileImg from 'assets/mobile-responsive-design.png'
 
 const Home = () => {
   const handleChangeTheme = () => {
@@ -13,7 +18,72 @@ const Home = () => {
   return (
     <Container>
       <Menu />
-      <ContentContainer handleChangeTheme={handleChangeTheme} />
+      <div>
+        <ParagraphSection>
+          <h2>
+            Crie este site <span>responsivo</span> com <span>react</span>{' '}
+            utilizando <span>styled-components</span>
+          </h2>
+
+          <h3>
+            A fonte utilizada é a Open Sans de 300 a 800.
+            <br />
+            exemplo: Open Sans, Helvetica, sans-serif, arial;
+            <br />
+            Já as cores são:
+            <br />
+            <Circle />
+          </h3>
+        </ParagraphSection>
+
+        <ImageSection>
+          <BoxImg background="#FE9481">
+            <div className="desktop">
+              <img src={DesktopImg} alt={DesktopImg} background="#FE9481" />
+              <h3>Site Responsivo desktop</h3>
+            </div>
+            <div>
+              <p>
+                Quando pressionado o botão <span>Leia mais...</span> o restante
+                da informação deverá aparecer em scroll down.
+              </p>
+              <Button background="#FE9481" title="Leia mais..." />
+            </div>
+          </BoxImg>
+
+          <BoxImg background="#FCDA92">
+            <div className="tablet">
+              <img src={TabletImg} alt={TabletImg} background="#FCDA92" />
+              <h3>Site Responsivo tablet</h3>
+            </div>
+            <div>
+              <p>
+                Quando pressionado o botão <span>Leia mais...</span> informação
+                deverá aparecer completa em um popup na tela.
+              </p>
+              <Button background="#FCDA92" title="Leia mais..." />
+            </div>
+          </BoxImg>
+
+          <BoxImg background="#9C8CB9">
+            <div className="mobile">
+              <img src={MobileImg} alt={MobileImg} background="#9C8CB9" />
+              <h3>Site Responsivo mobile</h3>
+            </div>
+            <div>
+              <p>
+                Quando pressionado o botão <span>alterar tema</span> modifique o
+                tema do site para blackfriday a seu gosto.
+              </p>
+              <Button
+                background="#9C8CB9"
+                handleAction={() => handleChangeTheme()}
+                title="Alterar tema"
+              />
+            </div>
+          </BoxImg>
+        </ImageSection>
+      </div>
       <Footer />
     </Container>
   )
--- src/containers/home/Styled.js
+++ src/containers/home/Styled.js
@@ -4,3 +4,89 @@ export const Container = styled.main`
   margin: auto;
   width: 1200px;
 `
+
+export const ParagraphSection = styled.section`
+  margin: 5% 10%;
+  h2 {
+    color: #007f56;
+    font-weight: 300;
+    font-size: 3.3333333333em;
+    text-align: center;
+
+    span {
+      font-weight: bold;
+
+      &:nth-child(even) {
+        text-transform: uppercase;
+      }
+    }
+  }
+
+  h3 {
+    color: #868686;
+    font-size: 1.7777777778em;
+    text-align: center;
+    font-weight: 300;
+    margin: 5%;
+  }
+`
+
+export const ImageSection = styled.section`
+  display: grid;
+  grid-template-columns: 31% 31% 31%;
+  grid-column-gap: 3%;
+`
+
+export const BoxImg = styled.div`
+  background: ${(props) => props.background};
+  max-height: 330px;
+
+  .desktop {
+    background: #fe9481;
+
+    button {
+      background: ${(props) => props.backgroud};
+    }
+  }
+  .tablet {
+    background: #fcda92;
+  }
+  .mobile {
+    background: #9c8cb9;
+  }
+
+  img {
+    background: ${(props) => props.background};
+    margin: 3% 20%;
+  }
+
+  div {
+    background: #fff;
+    min-height: 112px;
+  }
+
+  h3 {
+    background: ${(props) => props.background};
+    color: #fff;
+    font-size: 1.77777777778em;
+    font-weight: 300;
+    margin: 30px 0;
+    text-align: center;
+  }
+
+  p {
+    color: #868686;
+    background: #fff;
+    float: left;
+    height: 100px;
+    padding: 1.9444444444444445%;
+    max-width: 100%;
+    text-align: justify;
+    top: 225px;
+  }
+
+  span {
+    background: #fff;
+    font-weight: 800;
+  }
+`
